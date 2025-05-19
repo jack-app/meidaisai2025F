6f8680f97c28cc59e963c250136a5e86f5aa0c58
nunupy345345 <nunupy345345@airi9ri>
2025-05-19 20:33:53 +0900

    [add][page]

--- src/pages/Select.tsx
+++ src/pages/Select.tsx
@@ -9,6 +9,10 @@ export const Select = () => {
   return (
     <div className="select">
       <NavLink to={{ pathname: "/program", search: `?id=${id}&name=${name}` }}>プログラミングへ</NavLink>
+      <br />
+      <NavLink to={{ pathname: "/programAlto", search: `?id=${id}&name=${name}` }}>プログラミングあるとへ</NavLink>
+      <br />
+      <NavLink to={{ pathname: "/programTyupei", search: `?id=${id}&name=${name}` }}>プログラミングちゅうぺいへ</NavLink>
       <h1>選択</h1>
     </div>
   );
